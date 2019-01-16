fad4c5953b4969615f75696909dcd657ead081e4
abuduru <abdulg1995@gmail.com>
2019-01-16 18:24:57 -0500

    Updates

--- precache-manifest.e6f2eb41960491108fc8120b887b750c.js
+++ precache-manifest.e6f2eb41960491108fc8120b887b750c.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/react-resume/static/js/runtime~main.4e131b12.js"
   },
   {
-    "revision": "a8294c7aad2944a200a6",
-    "url": "/react-resume/static/js/main.a8294c7a.chunk.js"
+    "revision": "0a2498711d2d34678f18",
+    "url": "/react-resume/static/js/main.0a249871.chunk.js"
   },
   {
     "revision": "45a4b776ca085930f677",
     "url": "/react-resume/static/js/1.45a4b776.chunk.js"
   },
   {
-    "revision": "a8294c7aad2944a200a6",
+    "revision": "0a2498711d2d34678f18",
     "url": "/react-resume/static/css/main.0d3e294f.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/react-resume/static/css/1.6d89be46.chunk.css"
   },
   {
-    "revision": "a66c7863cf19ffe5f5022f79cde967a0",
+    "revision": "f79e935e46a8e2e7e1b526eeeec90091",
     "url": "/react-resume/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-resume/precache-manifest.8602ffe66458f0ce2b49ac62cbb33b0a.js"
+  "/react-resume/precache-manifest.e6f2eb41960491108fc8120b887b750c.js"
 );
 
 workbox.clientsClaim();
